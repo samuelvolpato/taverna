e9fb9564f4694505c372d716bdccf6480cc3801b
bruno-holanda15 <br.holanda15@gmail.com>
2019-10-13 12:18:48 -0300

    Adicionado paginas teste para otimizar quantidade de arquivos

---
+++ informatica_teste.php
@@ -0,0 +1,145 @@
+<html>
+<?php $pageTitle = "Taverna";?>
+<?php include_once('utils\head.php');?>
+
+<body>
+    <?php include_once('utils\produtosTeste.php'); ?>
+
+    <main class="container-fluid">
+        <div class="sticky-top">
+            <?php include_once('utils\header.php');?>
+
+        </div>
+
+
+        <!-- CAROUSEL DE DESTAQUES -->
+        <div class="container-fluid p-0">
+            <div id="carouselHome" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselHome" data-slide-to="1"></li>
+                    <li data-target="#carouselHome" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets\img\banner_cod.jpg" alt="Primeiro Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets\img\banner_presentes.jpg" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets\img\banner_moda.jpg" alt="Terceiro Slide">
+                    </div>
+                </div>
+                <div class="setasCarousel">
+                    <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
+                        <i class="fas fa-caret-left"></i>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
+                            <i class="fas fa-caret-right"></i>
+                        <span class="sr-only">Próximo</span>
+                    </a>
+                </div>
+            </div>
+    
+        </div>
+
+        <div class="container-fluid">
+            <section class="row p-2 mt-3 ">
+                <div class="col-12 d-flex barraChamada">
+                    <h3 class="p-0 m-0"><i class="fas fa-grin-alt"></i> .ofertas épicas!</h3>
+                </div>
+            </section>
+        </div>
+
+
+
+        <!-- CARDS PROMOCIONAIS -->
+
+        <section class="container my-3 p-0">
+
+            <div class="row m-0 px-0 py-2" style="width: 100%">
+           
+               
+                <?php foreach ($produtosTeste as $nomeTeste => $infosTeste) { ?>
+                  <?php if ($infosTeste[3] == "informatica") {     ?>
+                    <div class="col-lg-3 col-sm-6 m-0 p-3">
+                        <img src="<?php echo $infosTeste[0]; ?>"  class=" card-img-top img-games" alt="<?php echo $infosTeste[1]; ?>" width="500" heigth="500">
+                        <div class="row justify-content-center mt-2 px-2">
+                            <h5 class=" text-white"><?php echo $infosTeste[1]; ?></h5>
+                        </div>
+                        <div class="row p-0 mt-2 ">
+                            <ul class="d-flex flex-row justify-content-center text-white col">
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star-half-alt"></i></li>
+                                <li><i class="far fa-star"></i></li>
+                                <li><img src="assets\img\taverna_logo.svg" class="ml-2" style="height: 20px" alt="">
+                                <small class="text-white">taverna indica</small></li>
+                            </ul>
+                        </div>
+                        <div class="row p-0 mt-2 justify-content-around">
+                            <p class="text-white"><strong><?php echo $infosTeste[3]; ?> ofertas a partir de</strong></p>
+                        </div>
+                        <div class="row justify-content-center">
+                            <h2 class="text-white text-center">R$ <?php echo $infosTeste[2]; ?></h2>
+                        </div>
+                        <div class="row justify-content-center mt-2">
+                            <button class="btn btnPadrao" title="Confira Já!">Confira Já! <i class="fas fa-thumbs-up"></i></button>
+                        </div>
+                    </div>
+                  <?php } else{}; ?>  
+                <?php } ?>
+                
+        </section>
+
+        <div class="container-fluid">
+            <section class="row p-2 mt-3 ">
+                <div class="col-12 d-flex barraChamada">
+                    <h3 class="p-0 m-0"><i class="fas fa-envelope-open"></i> .fique por dentro!</h3>
+                </div>
+            </section>
+        </div>
+
+
+
+
+<!-- FORMULÁRIO NEWSLETTER -->
+<div class="container-fluid my-3">
+    <div class="row text-center my-3">
+        <h2 class="text-white col-12">Não deixe ninguém de fora de promoções e novidades imperdíveis!</h2>
+        <h2 class="text-white col-12">Avise um amigo e compartilhe! </h2>
+    </div>
+    <div class="row justify-content-center">
+        <form class="col-8">
+            <div class="form-group">
+                <input type="email" class="form-control form-control-lg bg-transparent text-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o seu E-mail">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control form-control-lg bg-transparent text-white" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o E-mail de seu amigo">
+            </div>
+            <div class="row justify-content-center">
+                <button type="submit" class="btn btn-lg btnPadrao ">Cadastrar!</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+        <!-- FORMULÁRIO NEWSLETTER -->
+        <div class="col-12 bg-secondary" style="height: 300px;">
+            <h1 class="text-white text-center">banner</h1>
+        </div>
+
+
+
+
+
+        </section>
+    </main>
+</body>
+
+<?php include_once('utils\footer.php');?>
+
+</html>
---
+++ utils/produtosTeste.php
@@ -0,0 +1,223 @@
+<?php
+
+$produtosTeste = [
+    "Harry Potter 3" => [
+        "assets/img/hp_livro.jpg",
+        "Harrry Potter e o tio lobo",
+         49.99,
+        "livro"
+    ],
+    "Hobbit" => [
+        "assets/img/hobbit_livro.jpg",
+        "Hobbit o anão de pé peludo",
+        39.99,
+        "livro"
+    ],
+    "Heroi perdido" => [
+        "assets/img/heroi_perdido.jpg",
+        "Heroi perdido",
+        46.99,
+        "livro"
+    ],
+    "Percy Jackson" => [
+        "assets/img/percy5.jpg",
+        "Percy Jackson e o pégasus azul  ",
+        39.99,
+        "livro"
+    ],
+    "Percy Jackson" => [
+        "assets/img/percy4.jpg",
+        "Percy Jackson e o ladrão de energia",
+        39.99,
+        "livro"
+    ],
+    "O nome do vento" => [
+        "assets/img/nome_do_vento.jpg",
+        "O nome do vento/ melhor livro do mundo",
+        70.00,
+        "livro"
+    ],
+    "O temor do sábio" => [
+        "assets/img/temor_do_sabio.jpg",
+        "O temor do sábio/ melhor livro do mundo",
+        70.00,
+        "livro"
+    ],
+    "Game of Thrones 1" => [
+        "assets/img/got_1.jpg",
+        "Game of Thrones/ pior livro do mundo",
+        2.00,
+        "livro"
+    ],
+    "Game of Thrones 2" => [
+        "assets/img/got_3.jpg",
+        "Game of Thrones/ pior livro do mundo",
+        1.00,
+        "livro"
+    ],
+    "Camiseta" => [
+        "assets/img/camiseta_1.jpg",
+        "Camiseta TOPEN",
+         59.99,
+        "moda"
+    ],
+    "Camiseta Uchiha" => [
+        "assets/img/camiseta_2.jpg",
+        "Camiseta Uchiha bom , é Uchiha vivo",
+        59.99,
+        "moda"
+    ],
+    "Camiseta GOT" => [
+        "assets/img/camiseta_3.jpg",
+        " Camiseta King in the North",
+        96.99,
+        "moda"
+    ],
+    "Camiseta Sayagin GOD" => [
+        "assets/img/camiseta_4.jpg",
+        "Camiseta Sayagin Blue  ",
+        59.99,
+        "moda"
+    ],
+    "Camiseta Cubo" => [
+        "assets/img/camiseta_5.jpg",
+        "Camiseta Cubo ",
+        59.99,
+        "moda"
+    ],
+    "Camiseta Vaza" => [
+        "assets/img/camiseta_6.jpg",
+        "Camiseta Naza Vaza seu Otário",
+        70.00,
+        "moda"
+    ],
+    "Teclado" => [
+        "assets/img/teclado.jpg",
+        "Teclado game profissional",
+         189.99,
+        "informatica"
+    ],
+    "Teclado Menor" => [
+        "assets/img/teclado_menor.jpg",
+        "Teclado menor profissional",
+        110.00,
+        "informatica"
+    ],
+    "Mouse" => [
+        "assets/img/mouse.jpg",
+        " Mouse profissional",
+        96.99,
+        "informatica"
+    ],
+    "Nintendo" => [
+        "assets/img/nintendo_switch.jpg",
+        "Nintendo Switch Muitooo bom",
+        2222.99,
+        "informatica"
+    ],
+    "Headset" => [
+        "assets/img/fone.jpg",
+        "Headset profissional",
+        100.00,
+        "informatica"
+    ],
+    "Monitor" => [
+        "assets/img/monitor_gamer.jpg",
+        "Monitor gamer para os seus desafios",
+        500.00,
+        "informatica"
+    ],
+    "Mouse pad" => [
+        "assets/img/mouse_pad01.jpg",
+        "Mouse pad muitooo loucooo galera",
+        15.00,
+        "informatica"
+    ],
+    "Mouse pad 2" => [
+        "assets/img/mouse_pad02.jpg",
+        "Mouse pad muitooo doido galera",
+        25.00,
+        "informatica"
+    ],
+    "God of War 3" => [
+        "assets/img/god_3.jpg",
+        "Jogo de PS4 God of War",
+         59.99,
+        "game"
+    ],
+    "Uncharted 4" => [
+        "assets/img/uncharted_4.jpg",
+        "Jogo de PS4 Uncharted 4",
+        100.00,
+        "game"
+    ],
+    "Mafia 3" => [
+        "assets/img/mafia_3.jpg",
+        " Jogo de PS4 Mafia 3",
+        96.99,
+        "game"
+    ],
+    "Horizon" => [
+        "assets/img/horizon.jpg",
+        "Jogo de PS4 Horizon ",
+        22.99,
+        "game"
+    ],
+    "God of War 4" => [
+        "assets/img/god_4.jpg",
+        "Jogo de PS4 God of War 4",
+        100.00,
+        "game"
+    ],
+    "Final Fantasy" => [
+        "assets/img/ff_15.jpg",
+        "Jogo de PS4 FF 15",
+        80.00,
+        "game"
+    ],
+    "Naruto" => [
+        "assets/img/naruto.jpg",
+        "Narutinho herói de Konoha",
+         129.99,
+        "colecionaveis"
+    ],
+    "Sasuke" => [
+        "assets/img/sasuke.jpg",
+        "Sasuke",
+        1.00,
+        "colecionaveis"
+    ],
+    "Shikamaru" => [
+        "assets/img/shikamaru.jpg",
+        " Shikamaru > Einstein",
+        199.99,
+        "colecionaveis"
+    ],
+    "Jiraiya" => [
+        "assets/img/jiraiya.jpg",
+        "Jiraiya ero-senin ",
+        222.99,
+        "colecionaveis"
+    ],
+    "Tanjiro" => [
+        "assets/img/tanjiro.jpg",
+        "Tanjiro pocket",
+        120.00,
+        "colecionaveis"
+    ],
+    "Nezuko" => [
+        "assets/img/mina.jpg",
+        "Nezuko fofinha",
+        120.00,
+        "colecionaveis"
+    ],
+    "Zenitsu e Inozuke" => [
+        "assets/img/zenitsu.jpg",
+        "Melhor dupla everr",
+        220.00,
+        "colecionaveis"
+    ]
+
+];
+
+?>
