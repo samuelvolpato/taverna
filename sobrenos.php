05186c27fc93127362ee9c6f026bff051504f643
Lucas Valotto <lucas.valotto@gmail.com>
2019-10-17 17:48:35 -0300

    estilos finais faq e sobre

--- sobrenos.php
+++ sobrenos.php
@@ -4,12 +4,12 @@
 <?php require_once("utils/header.php") ?>
     <main>
       
-      <div id="titulo-faq" class="container d-flex justify-content-center"> Sobre nós</div>
+      <div id="titulo-faq" class="container d-flex justify-content-center"><h1>Sobre nós</h1></div>
 
       <section class="section-sobre">
       
         <div class="sobre container col-12 col-md-6 d-flex justify-content-center">
-          <p class="p-texto d-flex justify-content-center"> Somos um site e-commerce com foco em produtos geeks, desde camisetas até jogos de video game e tabuleiros RPG.</p>
+          <h4 class="d-flex justify-content-center"> Somos um site e-commerce com foco em produtos geeks, desde camisetas até jogos de video game e tabuleiros RPG.</h4>
         </div>
         
 
