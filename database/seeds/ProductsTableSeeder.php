747ca12cde5e512dd09721c94090199af4c841ee
Lucas Valotto <lucas.valotto@gmail.com>
2019-12-07 18:33:10 -0300

    criado seeder de produtos, atualização da rota p/ cadastro no faq

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'nome' => 'Minato',
+            'codigo' => '00001',
+            'fabricante' => 'otakus',
+            'preco' => '100',
+            'descricao' => 'boneco minato colecionavel',
+            'detalhes' => '30 cm, durepox',
+            'img_path' => 'storage/app/img/minato.jpg',
+            'categoria' => 'colecionaveis'
+        ]);
+
+        Product::create([
+            'nome' => 'Naruto',
+            'codigo' => '00002',
+            'fabricante' => 'otakus',
+            'preco' => '99,99',
+            'descricao' => 'boneco naruto colecionavel',
+            'detalhes' => '29 cm, durepox',
+            'img_path' => 'storage/app/img/naruto.jpg',
+            'categoria' => 'colecionaveis'
+        ]);
+
+        Product::create([
+            'nome' => 'Sasuke',
+            'codigo' => '00001',
+            'fabricante' => 'otakus',
+            'preco' => '110',
+            'descricao' => 'boneco sasuke colecionavel',
+            'detalhes' => '27 cm, durepox',
+            'img_path' => 'storage/app/img/sasuke.jpg',
+            'categoria' => 'colecionaveis'
+        ]);
+
+    }
+}
